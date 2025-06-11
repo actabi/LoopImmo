513b80d5dcc8a2756dcde3562e6142ffb2836dd0
Guillaume De Laroque <gdelaroque@erama.co>
2025-06-11 17:16:21 +0200

    remove unused AmbassadorLeads page

--- src/pages/ambassador/AmbassadorLeads.tsx
+++
@@ -1,4 +0,0 @@
-import React, { useState } from 'react';
-import { DashboardLayout } from '../../components/dashboard/DashboardLayout';
-import { Card } from '../../components/ui/Card';
-import { Button } from '../../components/ui/Button';
