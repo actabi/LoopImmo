fddca6e20c7980d9824bf3f81845587c0aac0edc
Guillaume De Laroque <gdelaroque@erama.co>
2025-06-17 17:08:49 +0200

    Fix imports and add layout component

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LaunchPage } from './pages/LaunchPage';
 import { LandingPage } from './pages/LandingPage';
-import { LoginPage } from './pages/LoginPage';
-import { RegisterPage } from './pages/RegisterPage';
-import { BuyPage } from './pages/BuyPage';
-import { SellPage } from './pages/SellPage';
-import { AmbassadorPage } from './pages/AmbassadorPage';
+import { LoginPage } from './pages/auth/LoginPage';
+import { RegisterPage } from './pages/auth/RegisterPage';
+import { AcheterPage } from './pages/AcheterPage';
+import { VendrePage } from './pages/VendrePage';
+import { AmbassadeurPage } from './pages/AmbassadeurPage';
 import { PropertyDetailPage } from './pages/PropertyDetailPage';
-import { DashboardPage } from './pages/DashboardPage';
-import { ProfilePage } from './pages/ProfilePage';
-import { PropertiesPage } from './pages/PropertiesPage';
-import { VisitsPage } from './pages/VisitsPage';
-import { OffersPage } from './pages/OffersPage';
-import { MessagesPage } from './pages/MessagesPage';
-import { SettingsPage } from './pages/SettingsPage';
+import { UnifiedDashboard } from './pages/dashboard/UnifiedDashboard';
+import { RolesManagement } from './pages/profile/RolesManagement';
+import { PropertiesPage } from './pages/seller/PropertiesPage';
+import { VisitsPage } from './pages/seller/VisitsPage';
+import { OffersPage } from './pages/seller/OffersPage';
+import { MessagesPage } from './pages/seller/MessagesPage';
+import { SettingsPage } from './pages/seller/SettingsPage';
 import { Layout } from './components/Layout';
 import { AuthProvider } from './contexts/AuthContext';
-import { ProtectedRoute } from './components/ProtectedRoute';
+import { ProtectedRoute } from './components/auth/ProtectedRoute';
 
 function App() {
   // Lire la variable d'environnement pour déterminer quelle page afficher
@@ -33,15 +33,15 @@ function App() {
           {/* Autres pages publiques */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/acheter" element={<BuyPage />} />
-          <Route path="/vendre" element={<SellPage />} />
-          <Route path="/ambassadeur" element={<AmbassadorPage />} />
+          <Route path="/acheter" element={<AcheterPage />} />
+          <Route path="/vendre" element={<VendrePage />} />
+          <Route path="/ambassadeur" element={<AmbassadeurPage />} />
           <Route path="/bien/:id" element={<PropertyDetailPage />} />
           
           {/* Pages protégées avec Layout */}
           <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/dashboard" element={<UnifiedDashboard />} />
+            <Route path="/profile" element={<RolesManagement />} />
             <Route path="/mes-biens" element={<PropertiesPage />} />
             <Route path="/visites" element={<VisitsPage />} />
             <Route path="/offres" element={<OffersPage />} />
