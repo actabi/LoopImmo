deaf2df9dd62020a27697679cf44a683acbb3887
Guillaume De Laroque <gdelaroque@erama.co>
2025-06-18 22:06:17 +0200

    Handle self-signed DB certificates

--- server/src/db.ts
+++ server/src/db.ts
@@ -5,8 +5,12 @@ import dotenv from 'dotenv';
 // Load server-specific environment variables
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
+const connectionString = process.env.DATABASE_URL;
+const sslRequired = connectionString?.includes('sslmode=require');
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
+  ...(sslRequired ? { ssl: { rejectUnauthorized: false } } : {}),
 });
 
 export const query = (text: string, params?: any[]) => pool.query(text, params);
