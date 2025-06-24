a8f4da035d8f5eb54cdf53b21f43b0e07dd77bab
Guillaume De Laroque <gdelaroque@erama.co>
2025-06-24 09:28:35 +0200

    Add debug message for env loading

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,14 +1,14 @@
-export const isDebug = process.env.DEBUG === 'true';
+export const isDebug = () => process.env.DEBUG === 'true';
 
 export const log = (...args: unknown[]) => {
-  if (isDebug) {
+  if (isDebug()) {
     // eslint-disable-next-line no-console
     console.log(...args);
   }
 };
 
 export const error = (...args: unknown[]) => {
-  if (isDebug) {
+  if (isDebug()) {
     // eslint-disable-next-line no-console
     console.error(...args);
   }
