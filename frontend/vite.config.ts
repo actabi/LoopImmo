84ac3545569cd98df13d1c23e5cc13a5aa24fe1f
Guillaume De Laroque <gdelaroque@erama.co>
2025-08-12 22:10:56 +0200

    feat: add PWA scaffold

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: false,
+        includeAssets: ['logo.svg', 'logoS.svg']
+      })
+    ],
     server: {
       proxy: {
         '/api': env.VITE_API_URL || 'http://localhost:3000',
